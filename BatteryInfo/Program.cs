43cb80726a860d5000d50c04e35872b44afca075
LeoVLDM <LeoVLDM@users.noreply.github.com>
2020-02-08 00:31:29 +0300

    first commit

---
+++ BatteryInfo/Program.cs
@@ -0,0 +1,150 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        // Start Script
+
+        readonly bool error, bHydrogen, bGrinder;
+        readonly List<IMyBatteryBlock> lBatteryes;
+        readonly List<IMyProgrammableBlock> lProgram;
+        readonly List<IMyGasTank> lHydrogen;
+        readonly List<IMyShipDrill> lDrill;
+        readonly IMyTextSurface ProgDisp0, ProgDisp1;
+        
+        
+        public Program()
+        {
+            lBatteryes = new List<IMyBatteryBlock>();
+            GridTerminalSystem.GetBlocksOfType<IMyBatteryBlock>(lBatteryes, block => block.IsSameConstructAs(Me));
+            if (lBatteryes.Count == 0) {
+                Echo("Нет батарей!");
+                error = true;
+            }
+
+            lHydrogen = new List<IMyGasTank>();
+            GridTerminalSystem.GetBlocksOfType<IMyGasTank>(lHydrogen, block => block.IsSameConstructAs(Me));
+            if (lHydrogen.Count > 0) {
+                Echo("GasTanks: " + lHydrogen.Count);
+                bHydrogen = true;
+            }
+
+            lDrill = new List<IMyShipDrill>();
+            GridTerminalSystem.GetBlocksOfType<IMyShipDrill>(lDrill, block => block.IsSameConstructAs(Me));
+            if (lDrill.Count > 0) {
+                bGrinder = true;
+            }
+
+            lProgram = new List<IMyProgrammableBlock>();
+            GridTerminalSystem.GetBlocksOfType<IMyProgrammableBlock>(lProgram, block => block.IsSameConstructAs(Me));
+            if (lProgram.Count > 0) {
+                ProgDisp0 = lProgram[0].GetSurface(0);
+                ProgDisp0.FontSize = 1.8f;
+                ProgDisp0.BackgroundColor = Color.Black;
+                ProgDisp1 = lProgram[0].GetSurface(1);
+                ProgDisp1.FontSize = 5f;
+                ProgDisp1.BackgroundColor = Color.Black;
+            } else {
+                Echo("Нет программного блока!");
+                error = true;
+            }
+
+            if (error) {
+                Runtime.UpdateFrequency = UpdateFrequency.None;
+            } else {
+                Runtime.UpdateFrequency = UpdateFrequency.Update1;
+            }
+        }
+
+        public void Save()
+        {
+
+        }
+
+        public void Main(string argument, UpdateType updateSource) {
+            string sName, sDisp;
+            float fSPower, fCPowerP, fGas;
+            float fCurStorPower = 0.0f;
+            float fMaxStorPower = 0.0f;
+            float fCurInputPower = 0.0f;
+            float fCurOutputPower = 0.0f;
+            float fFilledHydrogen = 0.0f;
+            float fInventoryDrill = 0.0f;
+
+            foreach (IMyBatteryBlock battery in lBatteryes) {
+                sName = battery.DisplayNameText;
+                if (battery.IsFunctional) {
+                    fSPower = battery.CurrentStoredPower;
+                    fCurStorPower += fSPower;
+                    fMaxStorPower += battery.MaxStoredPower;
+                    fCurInputPower += battery.CurrentInput;
+                    fCurOutputPower += battery.CurrentOutput;
+
+                    Echo(sName + ": " + fSPower);
+                } else {
+                    Echo(sName + ": !no functional!");
+                }
+            }
+            fCPowerP = (fCurStorPower / fMaxStorPower * 100);
+
+            Echo("Макс заряд: " + fMaxStorPower + "\n");
+            Echo("Заряд: " + fCPowerP.ToString("0.00") + "%");
+            Echo("Потр: " + fCurInputPower.ToString("0.00"));
+            Echo("Отдача: " + fCurOutputPower.ToString("0.00"));
+
+            sDisp = "Батарея: " + fCPowerP.ToString("0") + "%\n";
+            sDisp += "<- " + fCurInputPower.ToString("0.00")
+                + " | " + fCurOutputPower.ToString("0.00") + " ->";
+
+            ProgDisp1.WriteText(sDisp);
+
+            if (fCPowerP >= 50) {
+                ProgDisp1.FontColor = Color.Lime;
+                //Echo(ProgDisp1.FontColor.ToString());
+            } else if (fCPowerP>=25) {
+                ProgDisp1.FontColor = Color.Yellow;
+            } else {
+                ProgDisp1.FontColor = Color.Red;
+            }
+
+            sDisp = "\n\n";
+            if (bHydrogen) {
+                foreach (IMyGasTank gas in lHydrogen) {
+                    //Echo(gas.GetType().ToString());
+                    //Echo(gas.DisplayNameText+": " + gas.Capacity);
+                    fGas = ((float)gas.FilledRatio * 100);
+                    sDisp += gas.DisplayNameText + ": " + fGas.ToString("0") + "%\n";
+                }
+            }
+
+            if (bGrinder) {
+                foreach(IMyShipDrill drill in lDrill) {
+                    sDisp += drill.DisplayNameText + ": " + drill.GetInventory().MaxVolume + "\n";
+                }
+            }
+
+            Echo(sDisp);
+            ProgDisp0.WriteText(sDisp);
+        }
+
+        // End Script
+    }
+}
---
+++ MinerBombs/Program.cs
@@ -0,0 +1,173 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript {
+	partial class Program : MyGridProgram {
+
+		// Скорость сварки боеголовки
+		int speed = 30;
+		// Скорость поршня
+		int speedPiston = 3;
+		
+
+		//Projector		//Welder		//Merge Block		//Warhead
+		IMyProjector myProjector;
+		List<IMyShipWelder> LmyWelders;
+		List<IMyShipMergeBlock> LmyMergeBlocks;
+		List<IMyWarhead> LmyWarhead;
+		//List<IMyPistonBase> LmyPistonBases;
+		
+
+		bool on;
+		int time;
+
+		public Program() {
+			myProjector = GridTerminalSystem.GetBlockWithName("Projector") as IMyProjector;
+
+			LmyWelders = new List<IMyShipWelder>();
+			LmyMergeBlocks = new List<IMyShipMergeBlock>();
+			LmyWarhead = new List<IMyWarhead>();
+			//LmyPistonBases = new List<IMyPistonBase>();
+
+			Runtime.UpdateFrequency = UpdateFrequency.Update10;
+
+			on = false;
+			time = 0;
+
+			//Main("Start", UpdateType.None);	// Автопуск
+		}
+
+		public void Save() {
+
+		}
+
+		public void Main(string argument, UpdateType updateSource) {
+			if (updateSource == UpdateType.Update10) {
+
+				Echo("Проектор включен? - " + myProjector.IsProjecting.ToString());
+				Echo("Скорость пуска: " + speed.ToString());
+				if (!myProjector.IsProjecting) {
+					on = false;
+				}
+
+				if (on == true) {
+					Echo("Блоков осталось: " + myProjector.RemainingBlocks.ToString());
+					Echo("Тик: " + time.ToString());
+					//if (time==10 && projector.RemainingBlocks==0) {
+					if (time==5) {
+						GridTerminalSystem.GetBlocksOfType<IMyWarhead>(LmyWarhead, block => block.IsSameConstructAs(Me));
+						Echo("Боеголовка ... ("+ LmyWarhead.Count + ")");
+						if (LmyWarhead.Count > 0) {
+							foreach (IMyWarhead warhead in LmyWarhead) {
+								if (warhead.IsFunctional && warhead.IsArmed == false) {
+									warhead.IsArmed = true;
+									Echo("\t взведена!");
+								}
+							}
+
+							GridTerminalSystem.GetBlocksOfType<IMyShipMergeBlock>(LmyMergeBlocks, block => block.IsSameConstructAs(Me));
+							foreach (IMyShipMergeBlock mergeBlock in LmyMergeBlocks) {
+								Echo("Блок соединён? - " + mergeBlock.IsConnected);
+								if (mergeBlock.IsConnected) {
+									mergeBlock.Enabled = false;
+									Echo("Пуск боеголовки!");
+								}
+							}
+
+						} else {
+							Echo("\t не готова!");
+							Main("Stop", UpdateType.None);
+							Main("Start", UpdateType.None);
+						}
+
+					}
+					//if (time==30 && projector.RemainingBlocks != 0) {
+					if (time==speed) {
+						GridTerminalSystem.GetBlocksOfType<IMyShipMergeBlock>(LmyMergeBlocks, block => block.IsSameConstructAs(Me));
+						foreach (IMyShipMergeBlock mergeBlock in LmyMergeBlocks) {
+							if (mergeBlock.IsFunctional) {
+								mergeBlock.Enabled = true;
+								Echo("Соединитель включён!");
+							}
+						}
+					}
+					if (time > speed) time = 0;
+					time += 1;
+				}
+			} else {
+				switch (argument) {
+					case "Start":
+						if (myProjector.IsProjecting) {
+							Echo("Запускаем сварку");
+							GridTerminalSystem.GetBlocksOfType<IMyShipWelder>(LmyWelders, block => block.IsSameConstructAs(Me));
+							foreach (IMyShipWelder welder in LmyWelders) {
+								if (welder.IsFunctional) {
+									welder.Enabled = true;
+								}
+							}
+							Echo("Включаем соединитель");
+							GridTerminalSystem.GetBlocksOfType<IMyShipMergeBlock>(LmyMergeBlocks, block => block.IsSameConstructAs(Me));
+							if (LmyMergeBlocks.Count > 0) {
+								foreach (IMyShipMergeBlock mergeBlock in LmyMergeBlocks) {
+									mergeBlock.Enabled = true;
+								}
+							}
+							on = true;
+						} else {
+							Echo("Проектор выключен!");
+						}
+						break;
+
+					case "Stop":
+						Echo("Останавливаем сварку");
+						GridTerminalSystem.GetBlocksOfType<IMyShipWelder>(LmyWelders, block => block.IsSameConstructAs(Me));
+						foreach (IMyShipWelder welder in LmyWelders) {
+							if (welder.IsFunctional) {
+								welder.Enabled = false;
+							}
+						}
+						Echo("Отключаем соединитель");
+						GridTerminalSystem.GetBlocksOfType<IMyShipMergeBlock>(LmyMergeBlocks, block => block.IsSameConstructAs(Me));
+						if (LmyMergeBlocks.Count > 0) {
+							foreach (IMyShipMergeBlock mergeBlock in LmyMergeBlocks) {
+								mergeBlock.Enabled = false;
+							}
+						}
+						on = false;
+						break;
+
+					case "Speed+":
+						speed -= 1;
+						if (speed < 10) speed = 10;
+						break;
+
+					case "Speed-":
+						speed += 1;
+						if (speed > 50) speed = 50;
+						break;
+
+					default:
+						break;
+				}
+
+			}
+
+		}
+	}
+}
